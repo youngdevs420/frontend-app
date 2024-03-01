f13d393f9c1ad0e911681e03c74ff6a456346cfd
youngdevs420 <youngdevs420@gmail.com>
2024-03-01 02:14:15 -0800

    Send message form

--- src/views/dashboard/pages/insureds/components/InsuredInfo.vue
+++ src/views/dashboard/pages/insureds/components/InsuredInfo.vue
@@ -97,17 +97,18 @@
                                         <label>States Hauling to/from</label>
                                         <input type="text" class="form-control mt-2" value="Separate by comma">
                                     </div>
-                                    
+
                                     <div class="form-group col-sm-3 mt-3">
                                         <label>Radius</label>
                                         <input type="text" class="form-control mt-2" value="Radius">
                                     </div>
                                     <div class="form-group col-sm-4 mt-3">
                                         <label>Commodities Hauled (Be Specific)</label>
-                                        <input type="text" class="form-control mt-2" value="Commodities Hauled (Be Specific)">
+                                        <input type="text" class="form-control mt-2"
+                                            value="Commodities Hauled (Be Specific)">
                                     </div>
 
-                                    
+
                                     <div class="form-group col-sm-2 mt-3 ps-1">
                                         <label>State Filings*?</label>
                                         <div class="d-flex align-items-center">
@@ -122,7 +123,8 @@
 
                                     <div class="form-group col-sm-12 mt-3">
                                         <label>How did you hear about us?</label>
-                                        <input type="text" class="form-control mt-2" value="Facebook, Instagram, Referral, etc">
+                                        <input type="text" class="form-control mt-2"
+                                            value="Facebook, Instagram, Referral, etc">
                                     </div>
                                 </div>
                             </div>
@@ -131,8 +133,57 @@
                 </div>
             </div>
             <div class="col-xl-3">
-                <div class="i-chat-card p-3">
+                <div class="i-chat-card card py-3 border-0">
+                    <div class="ic-header">
+                        <h5 class="px-3">Conversation with Insured</h5>
+                        <hr>
+                    </div>
+                    <div class="ic-body px-3">
+                        <div class="r-msg-box card py-3 px-3">
+                            <div class="user d-flex align-items-center mb-3">
+                                <img :src="'/assets/media/users/u1.png'" height="30" width="30" alt="user">
+                                <small class="ps-2">Hey John!</small>
+                            </div>
+                            <div class="msg-info">
+                                <small>Hey John!
+                                    As a reminder, there’s issues that should be addressed in:
+                                    Email Security
+                                    Endpoint Security</small>
+                                    <ul>
+                                        <li><small>Email Security</small></li>
+                                        <li><small>Endpoint Security</small></li>
+                                    </ul>
+                            </div>
+                        </div>
+                        <div class="sent-msg my-3">
+                            <div class="card border-0 msg-box bg-blue ms-auto p-3">
+                                <img :src="'/assets/media/users/u2.png'" height="28" width="28" alt="user">
+                                <small>Run another risk assessment</small>
+                            </div>
+                            <p class="d-flex align-items-end justify-content-end mt-2">
+                                <small class="txt-muted">Robert  12/20/23  9:30AM </small>
+                            </p>
+                        </div>
+                        <div class="r-msg-box card py-3 px-3">
+                            <div class="user d-flex align-items-center mb-3">
+                                <img :src="'/assets/media/users/u1.png'" height="30" width="30" alt="user">
+                                <small class="ps-2">Hey John!</small>
+                            </div>
+                            <div class="msg-info">
+                                <small>How are you doing?</small>
+                            </div>
+                        </div>
 
+                        <form id="sent-msg-form" class="mt-3">
+                            <div class="form-group card pe-2 py-1">
+                                <input type="text" class="form-control w-75 border-0" placeholder="Type something...">
+                                <button class="btn-blue-sm py-0">
+                                    <img :src="'/assets/media/icons/send.png'" height="24" width="24" alt="user">
+
+                                </button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
@@ -149,5 +200,11 @@
     min-height: 200px;
     border-radius: 16px;
     background: #F9FAFB;
+}
+
+#sent-msg-form .card {
+    display: flex !important;
+    flex-direction: row !important;
+    justify-content: space-between;
 }
 </style>
